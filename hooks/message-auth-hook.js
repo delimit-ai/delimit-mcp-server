d3fb9b97b3086e5a6bff09266b728fede6e721d9
infracore <jamsonsholdings@gmail.com>
2026-03-16 11:33:29 -0400

    chore: remove stale hooks/message-auth-hook.js

--- hooks/message-auth-hook.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-// Triggers auth setup on keywords
-const keywords = ['setup credentials', 'github key', 'api key'];
-const message = process.argv[2] || '';
-
-if (keywords.some(k => message.toLowerCase().includes(k))) {
-    console.log('[DELIMIT] Authentication setup triggered');
-    require('child_process').execSync('delimit auth');
-}
